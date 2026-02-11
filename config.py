74591f8b8a27bd3af9fd48961a6a6435fadc7cad
elzanhemila10-sudo <elzanhemila10@gmail.com>
2026-02-12 01:40:47 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "8045489007:AAEx2ev5tHMuMQO323NIKMaXstc9J379x68"
+ADMIN_ID = 6555089031
+
+REFERRAL_BONUS = 10
+PAGE_SIZE = 10
+
+PREMIUM = {
+    "bronze": {"days":30},
+    "silver": {"days":60},
+    "gold": {"days":90}
+}
